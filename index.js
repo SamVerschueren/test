208f20eacbc381165865256d37db5aa6c288caae
SamVerschueren <sam.verschueren@gmail.com>
2024-02-28 14:26:42 +0100

    Render html

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const http = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const code = `
+  <div id="foo"></div>
+  <script>
+    let counter = 0;
+
+    try {
+      counter = Number(localStorage.getItem('count') ?? '0')
+    } catch {}
+
+    counter++;
+
+    document.getElementById('foo').innerText = counter;
+
+    localStorage.setItem('count', counter);
+  </script>
+`;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(code);
 });
 
 server.listen(port, hostname, () => {
